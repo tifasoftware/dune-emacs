7cbe66b6b8f3da0b821af87069c35c03f51dbe36
infilit <tifasoftware@gmx.com>
2024-04-02 17:40:50 -0500

    updated init.el and .gitignore

--- init.el
+++ init.el
@@ -8,7 +8,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("d02a48a43c5f299d193a153fbd9c51eab3e3fd169f89cab691a41cc819eea9dc" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "0c860c4fe9df8cff6484c54d2ae263f19d935e4ff57019999edbda9c7eda50b8" default)))
+   '("d02a48a43c5f299d193a153fbd9c51eab3e3fd169f89cab691a41cc819eea9dc" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "0c860c4fe9df8cff6484c54d2ae263f19d935e4ff57019999edbda9c7eda50b8" default))
+)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
